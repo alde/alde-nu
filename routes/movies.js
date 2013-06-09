a07c8b8af2a34b1f5fa7ccc11263cc0dd88f570d
Rickard Dybeck <r.dybeck@gmail.com>
2013-06-09 22:23:41 +0200

    Update layout, only show adding/deleting movies for admin which is hard-coded to IP currently

--- routes/movies.js
+++ routes/movies.js
@@ -3,11 +3,17 @@ var mongoose = require('mongoose'),
 
 module.exports = function(app) {
     app.get('/movies', function(req, res) {
+        var ip = req.header('x-forwarded-for') || req.ip,
+            admin = false;
+
+        if (ip === "78.73.129.204") admin = true;
+
         Movies.find(function( err, movies, count ) {
             res.render(
                 'movies', {
                 title: 'Alde.nu - List of movies',
-                movies: movies
+                movies: movies,
+                admin: admin
             });
         });
     });
